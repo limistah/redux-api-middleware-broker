87708988d532d4dca4543cee4fcc34f63d2723ac
Aleem Isiaka <aleemisiaka@gmail.com>
2019-10-28 22:09:38 +0100

    Adds libs

---
+++ dist/umd.min.index.js
@@ -0,0 +1,3 @@
+!(function(n) {
+  "function" == typeof define && define.amd ? define(n) : n();
+})(function() {});
---
+++ rollup.config.js
@@ -0,0 +1,25 @@
+// rollup.config.js
+import resolve from "rollup-plugin-node-resolve";
+import commonjs from "rollup-plugin-commonjs";
+import babel from "rollup-plugin-babel";
+import { uglify } from "rollup-plugin-uglify";
+const plugins = [
+  resolve(),
+  commonjs(),
+  babel({
+    exclude: "node_modules/**" // only transpile our source code
+  })
+];
+const types = ["iife", "iife.min", "cjs", "cjs.min", "umd", "umd.min"];
+export default types.map(type => {
+  const _useUglify = type.includes("min") ? [uglify()] : [];
+  return {
+    input: "src/index.js",
+    output: {
+      file: `./dist/${type}.index.js`,
+      format: type.split(".")[0],
+      name: "bundle"
+    },
+    plugins: [...plugins, ..._useUglify]
+  };
+});
