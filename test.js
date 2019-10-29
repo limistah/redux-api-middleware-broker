423075210082761e27f3c3db75e774106936d1ad
Aleem Isiaka <aleemisiaka@gmail.com>
2019-10-29 07:57:16 +0100

    Adds a test file

--- test.js
+++ test.js
@@ -1,22 +1,11 @@
-const reduxApiMiddlewareBroker = require("../dist");
+const fetch = () => {};
+global.fetch = fetch;
+const reduxApiMiddlewareBroker = require("./dist");
 
 const action = reduxApiMiddlewareBroker({
   types: ["MAIN_ACTION_TYPE", "SUCCESS_ACTION_TYPE", "ERROR_ACTION_TYPE"],
   method: "GET",
   endpoint: `/users`
 });
-
-const obj = {
-  "@@redux-api-middleware/RSAA": {
-    endpoint: "/users",
-    method: "GET",
-    types: [
-      "MAIN_ACTION_TYPE",
-      { type: "SUCCESS_ACTION_TYPE" },
-      { type: "ERROR_ACTION_TYPE" }
-    ],
-    headers: {}
-  }
-};
 
 console.log(JSON.stringify(action));
